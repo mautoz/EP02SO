8311137bc417578d2990b9345fdf9b7076cd412b
mautoz <mauricio.ozaki@usp.br>
2018-11-25 23:26:42 -0200

    Novos arquivos

---
+++ src/Writer.java
@@ -0,0 +1,23 @@
+/*
+    Classe que representa Escritor
+    Baseado em:
+    https://wiki.eecs.yorku.ca/course_archive/2007-08/F/6490A/readers-writers
+ */
+public class Writer implements Runnable {
+
+    @Override
+    public void run() {
+
+        //entrar na base
+
+        //fazer 100 escritas
+        /*
+            Para cada acesso aleatorio na base escrever "MODIFICADO" na posicao
+            depois do ultimo acesso, antes de sair da base, deve esperar 1ms
+            NOTA: essa thread nao deve acabar dps da principal
+            usar join()
+         */
+
+
+    }
+}
